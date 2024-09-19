c07b32d163d3746dece9a1640132a17e3b273f56
MaylinLiu <maylinhliu@gmail.com>
2024-09-18 21:54:49 -0700

    Update ClockDriver.java

--- src/main/java/ClockDriver.java
+++ src/main/java/ClockDriver.java
@@ -1,44 +1,69 @@
+public class ClockDriver {
+    public static void main(String[] args) {
+        // Tests for ClockDisplay
+        System.out.println("Testing ClockDisplay:");
+        ClockDisplay clock1 = new ClockDisplay();  // Test empty constructor
+        System.out.println("Initial time (empty constructor): " + clock1.getTime());
 
-public class ClockDriver
-{
-    public static void main(String[] args){
-        //Implement tests for your ClockDisplay
-        //  * Test that you can do the basics
-        //      * Empty constructor & read time
-        //      * Constructor w/ time given & read time
-        //      * Set time
-        //      * Read time
-        //      * Tick time
-        //  * Tick test for 03:32 to 03:33
-        //  * Tick test for 03:09 to 03:10
-        //  * Tick test for 01:59 to 02:00
-        //  * Tick test for 09:59 to 10:00
-        //  * Tick test for 23:59 to 00:00
-        
-        
-        //Implement tests for your ClockDisplaySeconds
-        //  * Test that you can do the basics
-        //      * Empty constructor & read time
-        //      * Constructor w/ time given & read time
-        //      * Set time
-        //      * Read time
-        //      * Tick time
-        //  * Tick test for 03:32:59 to 03:33:00
-        //  * Tick test for 01:00:59 to 01:01:00
-        //  * Tick test for 01:59:59 to 02:00:00
-        //  * Tick test for 23:59:59 to 00:00:00
-        
-        /*CHALLENGE*/
-        //Implement tests for your ClockDisplay12Hour
-        //  * Test that you can do the basics
-        //      * Empty constructor & read time
-        //      * Constructor w/ time given & read time
-        //      * Set time
-        //      * Read time
-        //      * Tick time
-        //  * Tick test for 03:32PM to 03:33PM
-        //  * Tick test for 11:59PM to 12:00AM
-        //  * Tick test for 11:59AM to 12:00PM
-        //  * Tick test for 12:59PM to 01:00PM
+        ClockDisplay clock2 = new ClockDisplay(3, 32);  // Test constructor with time
+        System.out.println("Initial time (03:32 constructor): " + clock2.getTime());
+
+        clock2.setTime(3, 9);  // Test setting time
+        System.out.println("Time set to 03:09: " + clock2.getTime());
+
+        clock2.timeTick();  // Test ticking from 03:09 to 03:10
+        System.out.println("Tick from 03:09 to 03:10: " + clock2.getTime());
+
+        clock2.setTime(1, 59);  // Test ticking from 01:59 to 02:00
+        clock2.timeTick();
+        System.out.println("Tick from 01:59 to 02:00: " + clock2.getTime());
+
+        clock2.setTime(9, 59);  // Test ticking from 09:59 to 10:00
+        clock2.timeTick();
+        System.out.println("Tick from 09:59 to 10:00: " + clock2.getTime());
+
+        clock2.setTime(23, 59);  // Test ticking from 23:59 to 00:00
+        clock2.timeTick();
+        System.out.println("Tick from 23:59 to 00:00: " + clock2.getTime());
+
+        // Tests for ClockDisplaySeconds
+        System.out.println("\nTesting ClockDisplaySeconds:");
+        ClockDisplaySeconds clockSec1 = new ClockDisplaySeconds();  // Test empty constructor
+        System.out.println("Initial time with seconds (empty constructor): " + clockSec1.getTime());
+
+        ClockDisplaySeconds clockSec2 = new ClockDisplaySeconds(3, 32, 59);  // Test constructor with time
+        System.out.println("Initial time (03:32:59 constructor): " + clockSec2.getTime());
+
+        clockSec2.setTime(1, 0, 59);  // Test ticking from 01:00:59 to 01:01:00
+        clockSec2.timeTick();
+        System.out.println("Tick from 01:00:59 to 01:01:00: " + clockSec2.getTime());
+
+        clockSec2.setTime(1, 59, 59);  // Test ticking from 01:59:59 to 02:00:00
+        clockSec2.timeTick();
+        System.out.println("Tick from 01:59:59 to 02:00:00: " + clockSec2.getTime());
+
+        clockSec2.setTime(23, 59, 59);  // Test ticking from 23:59:59 to 00:00:00
+        clockSec2.timeTick();
+        System.out.println("Tick from 23:59:59 to 00:00:00: " + clockSec2.getTime());
+
+        // Tests for ClockDisplay12Hour
+        System.out.println("\nTesting ClockDisplay12Hour:");
+        ClockDisplay12Hour clock12Hr1 = new ClockDisplay12Hour();  // Test empty constructor
+        System.out.println("Initial 12-hour time (empty constructor): " + clock12Hr1.getTime());
+
+        ClockDisplay12Hour clock12Hr2 = new ClockDisplay12Hour(3, 32);  // Test constructor with time
+        System.out.println("Initial 12-hour time (03:32 PM constructor): " + clock12Hr2.getTime());
+
+        clock12Hr2.setTime(11, 59);  // Test ticking from 11:59 PM to 12:00 AM
+        clock12Hr2.timeTick();
+        System.out.println("Tick from 11:59 PM to 12:00 AM: " + clock12Hr2.getTime());
+
+        clock12Hr2.setTime(11, 59);  // Test ticking from 11:59 AM to 12:00 PM
+        clock12Hr2.timeTick();
+        System.out.println("Tick from 11:59 AM to 12:00 PM: " + clock12Hr2.getTime());
+
+        clock12Hr2.setTime(12, 59);  // Test ticking from 12:59 PM to 01:00 PM
+        clock12Hr2.timeTick();
+        System.out.println("Tick from 12:59 PM to 01:00 PM: " + clock12Hr2.getTime());
     }
 }
